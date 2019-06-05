3b500ccece03fa196f7f92191ad3d0efc0bb97ed
jconiff87 <51424223+jconiff87@users.noreply.github.com>
2019-06-05 17:12:50 -0500

    Add files via upload
    Test Upload

---
+++ OloTest/OloTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace OloTest
+{
+    public class Topping
+    {
+        public List<string> toppings = new List<string>();
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 1;
+            JsonConvert.DeserializeObject<List<Topping>>(new WebClient().DownloadString("http://files.olo.com/pizzas.json"))
+                .SelectMany(m => m.toppings)
+                .GroupBy(m => m)
+                .OrderByDescending(m => m.Count())
+                .Take(20)
+                .ToList()
+                .ForEach(m => 
+                {
+                    Console.WriteLine(string.Format("{0}. {1}: {2}", counter, m.FirstOrDefault().ToString(), m.Count()));
+                    counter++;
+                });
+
+            Console.ReadLine();
+        }
+    }
+}
